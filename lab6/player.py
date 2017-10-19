c8999f47a34b236cfa9e7529e8cb6f5415ff180f
ks100 <kavan100@gmail.com>
2017-10-19 17:27:18 +0100

    Add files via upload

---
+++ lab6/player.py
@@ -0,0 +1,8 @@
+from items import *
+from map import rooms
+
+inventory = [item_id, item_laptop, item_money]
+carry_weight = 4
+current_carry_weight = 2.51
+# Start game at the reception
+current_room = rooms["Reception"]
